161dd7dfc1b3a34f66031023f1e1677a1b013221
lemon-sh <pawel.stolarski7@gmail.com>
2021-12-09 09:44:09 +0100

    Fix a very embarassing send_dm bug

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+// exceptions for when `clippy::pedantic` is *too* pedantic
 #![allow(
     clippy::non_ascii_literal, // kurwa!
     clippy::too_many_arguments, // uhh great, but how tf do you expect me to do it otherwise?
-    clippy::unreadable_literal // it's not unreadable, it's a fucking hex color
+    clippy::unreadable_literal, // it's not unreadable, it's a hex color
+    clippy::too_many_lines, // YOU FORMATTED IT
+    clippy::match_on_vec_items // it won't panic, because I tested the length right before the match
 )]
 
 use std::{collections::HashMap, env, fmt::Display, sync::Arc, time::Duration};
@@ -24,6 +27,7 @@ use twilight_standby::Standby;
 const CIRCLE_EMOJIS: &[&str] =
     &["\u{1f535}", "\u{1f7e1}", "\u{1f7e2}", "\u{1f534}", "\u{26aa}", "\u{1f7e0}", "\u{1f7e4}", "\u{1f7e3}"];
 const YESNO_EMOJIS: &[&str] = &["\u{1f975}", "\u{1f976}"];
+const CANCEL_EMOJI: &str = "\u{274c}";
 
 lazy_static! {
     static ref HELP_EMBED: [Embed; 1] = [Embed {
@@ -36,7 +40,7 @@ lazy_static! {
                 "\u{2800}\u{2800}\u{30fb} `lbot help`\n",
                 "\u{2800}\u{25AB} Głosowanie\n",
                 "\u{2800}\u{2800}\u{30fb} `lbot wybory <id kanału> <czas> <wzmianki...>`\n",
-                "\u{2800}\u{2800}\u{30fb} `lbot referendum <id kanału> <czas> <typ>`"
+                "\u{2800}\u{2800}\u{30fb} `lbot referendum <id kanału> <czas> <ankieta|tn>`"
             )
             .to_string()
         ),
@@ -149,7 +153,7 @@ async fn execute_poll<'a, T: AsRef<str> + Display + PartialEq<String>>(
     };
     let poll_msg = http.create_message(channel_id).embeds(&[poll_embed])?.exec().await?.model().await?;
 
-    for reaction in reactions.iter().take(options.len()) {
+    for reaction in reactions.iter().take(options.len()).chain(&[CANCEL_EMOJI]) {
         http.create_reaction(poll_msg.channel_id, poll_msg.id, &RequestReactionType::Unicode { name: reaction })
             .exec()
             .await?;
@@ -158,6 +162,7 @@ async fn execute_poll<'a, T: AsRef<str> + Display + PartialEq<String>>(
         standby.wait_for_reaction_stream(poll_msg.id, move |v: &ReactionAdd| v.user_id != poll_msg.author.id);
     let collector = async {
         http.create_message(original_channel).content("Głosowanie zostało utworzone!")?.exec().await?;
+        log::info!("Poll '{}' has been created, listening for reactions", title);
         while let Some(reaction) = reaction_stream.next().await {
             http.delete_reaction(
                 reaction.channel_id,
@@ -170,10 +175,12 @@ async fn execute_poll<'a, T: AsRef<str> + Display + PartialEq<String>>(
             if let ReactionType::Unicode { name } = &reaction.emoji {
                 if let Some(o) = reactions.iter().take(options.len()).position(|&v| v == name) {
                     if selfvote_block && options[o] == format!("<@!{}>", reaction.user_id) {
-                        send_dm(http, reaction.user_id, "**Błąd:** Nie możesz głosować na siebie.").await?;
+                        log::info!("User {} tried to vote for themselves!", reaction.user_id);
+                        let _ = send_dm(http, reaction.user_id, "**Błąd:** Nie możesz głosować na siebie.").await;
                         continue;
                     }
-                    send_dm(
+                    log::info!("User {} voted for {}", reaction.user_id, options[o]);
+                    let _ = send_dm(
                         http,
                         reaction.user_id,
                         if let Some(v) = votes.insert(reaction.user_id, o) {
@@ -183,7 +190,13 @@ async fn execute_poll<'a, T: AsRef<str> + Display + PartialEq<String>>(
                         }
                         .as_str(),
                     )
-                    .await?;
+                    .await;
+                } else if name == CANCEL_EMOJI {
+                    if reaction.user_id == authorid {
+                        return Ok(())
+                    } else {
+                        let _ = send_dm(http, reaction.user_id, "**Błąd:** Niewystarczające uprawnienia.").await;
+                    }
                 }
             }
         }
@@ -200,17 +213,16 @@ async fn execute_poll<'a, T: AsRef<str> + Display + PartialEq<String>>(
         vote_count_full += 1;
         vote_counts[vote.1] += 1;
     }
+    log::info!("Poll {} finished, {} votes", title, vote_count_full);
     if vote_count_full > 0 {
         write!(&mut embed_description, "Ilość głosów: {}\n\n", vote_count_full)?;
         vote_counts.sort_by(|a, b| b.cmp(a));
         for vote_count in vote_counts.iter().enumerate() {
-            writeln!(
-                &mut embed_description,
-                "\u{2800}\u{30fb} {}: {}% ({} gł.)",
-                options[vote_count.0],
-                vote_count.1 * 100 / vote_count_full,
-                vote_count.1
-            )?;
+            let option = &options[vote_count.0];
+            let percent = vote_count.1 * 100 / vote_count_full;
+            let votes = vote_count.1;
+            log::info!("{}: {}% ({} votes)", option, percent, votes);
+            writeln!(&mut embed_description, "\u{2800}\u{30fb} {}: {}% ({} gł.)", option, percent, votes)?;
         }
     } else {
         embed_description.push_str("Brak głosów.");
@@ -232,7 +244,11 @@ async fn execute_poll<'a, T: AsRef<str> + Display + PartialEq<String>>(
     };
     http.delete_all_reactions(poll_msg.channel_id, poll_msg.id).exec().await?;
     http.update_message(poll_msg.channel_id, poll_msg.id).embeds(&[results_embed])?.exec().await?;
-    http.create_message(poll_msg.channel_id).content("Głosowanie zostało zakończone!")?.reply(poll_msg.id).exec().await?;
+    http.create_message(poll_msg.channel_id)
+        .content("Głosowanie zostało zakończone!")?
+        .reply(poll_msg.id)
+        .exec()
+        .await?;
     Ok(())
 }
 
@@ -332,7 +348,10 @@ async fn handle_message(msg: &MessageCreate, http: &Client, standby: Arc<Standby
                         options.push(opt.content.clone());
                     }
                     if options.is_empty() {
-                        http.create_message(msg.channel_id).content("**Błąd:** Nie podano żadnych opcji.")?.exec().await?;
+                        http.create_message(msg.channel_id)
+                            .content("**Błąd:** Nie podano żadnych opcji.")?
+                            .exec()
+                            .await?;
                         return Ok(());
                     }
                     execute_poll(
